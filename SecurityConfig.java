2fc08e3860db59995a99cc67a7ef41b14791e641
niketa741 <niketa741@gmail.com>
2025-08-03 03:45:22 +0530

    Changes to SecurityConfig to permitAll

---
+++ SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.referloop.referralservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/referrals/**","/hello").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable) // ✅ new syntax
+                .formLogin(AbstractHttpConfigurer::disable) // disable login UI
+                .httpBasic(AbstractHttpConfigurer::disable); // disable basic auth
+
+        return http.build();
+    }
+}
