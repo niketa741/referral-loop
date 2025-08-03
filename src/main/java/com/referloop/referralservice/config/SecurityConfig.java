61b7796d7ccdfb6233727beeb848108758ffbd28
niketa741 <niketa741@gmail.com>
2025-08-03 20:46:32 +0530

    Adding stateless to spring security

--- src/main/java/com/referloop/referralservice/config/SecurityConfig.java
+++ src/main/java/com/referloop/referralservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig {
@@ -12,14 +13,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/referrals/**","/hello")
-                        .permitAll()
-                        .anyRequest().authenticated()
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .csrf(AbstractHttpConfigurer::disable) // ✅ new syntax
-                .formLogin(AbstractHttpConfigurer::disable) // disable login UI
-                .httpBasic(AbstractHttpConfigurer::disable); // disable basic auth
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/referrals/**", "/hello").permitAll()
+                        .anyRequest().permitAll() // allow everything for now
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
